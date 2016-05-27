1c72f30927beef4c1e3cba3c02537f3c67c53e93
albyh <albyheredia@gmail.com>
2016-05-27 08:40:28 -0700

    refactored save to markerData to remove duplication if cached data loaded.

--- js/facility_db.js
+++ js/facility_db.js
@@ -25,7 +25,7 @@ var FacilityDb = function () {
   };
 
   this.Facility = function (facility, cached) {
-    this.name = facility[ cached ? 9 : 8];
+    this.name = facility[ cached ? 9 : 8]; //array position is different for cached data but only for facility name
     this.lat = parseFloat(facility[ cached ? 19 : 19]);
     this.lng = parseFloat(facility[ cached ? 18 : 18]);
     this.totBeds = Math.floor(facility[ cached ? 23 : 23]);
@@ -50,22 +50,11 @@ var FacilityDb = function () {
 
     console.groupCollapsed('Parse Marker Data Debugging');
 
-    if (facilityJson) {
-
       console.log('JSON received');
-      markerData = _.reduce(facilityJson.data, function (facilityObj, facility) {
+      markerData = _.reduce(facilityJson ? facilityJson.data : cachedFacilityData(), function (facilityObj, facility) {
         facilityObj[facility[1]] = new facilityDb.Facility(facility);
         return facilityObj;
       }, {});
-
-    } else {
-
-      console.warn('no JSON received. Loading cached facility data.');
-      markerData = _.reduce(cachedFacilityData(), function (facilityObj, facility) {
-        facilityObj[facility[1]] = new facilityDb.Facility(facility, true);
-        return facilityObj;
-      }, {});
-    }
 
     console.dir(markerData);
     console.groupEnd('Parse Marker Data Debugging');
